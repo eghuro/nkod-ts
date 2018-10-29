8d534033c28554c93fb7b7fe4caead82456f0f20
Alexandr Mansurov <alex@eghuro.cz>
2018-10-30 00:02:15 +0100

    Simple resource analyzer

---
+++ tsa/analyzer.py
@@ -0,0 +1,24 @@
+import json
+import uuid
+from collections import defaultdict
+from rdflib.namespace import RDF
+from tsa.extensions import redis
+
+class Analyzer(object):
+
+    def analyze(self, graph):
+        triples = len(graph)
+        predicates_count = defaultdict(int)
+        classes = set()
+        for s, p, o in graph:
+            predicates_count[p] = predicates_count[p] + 1
+            if p == RDF.type:
+                classes.add(o)
+
+        summary = {
+            'triples': triples,
+            'predicates': predicates_count,
+            'classes': list(classes)
+        }
+
+        return summary
