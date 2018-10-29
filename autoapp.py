9645d1d9871dfca70f832fdaecf7990915fbd3f5
Alexandr Mansurov <alex@eghuro.cz>
2018-10-30 00:04:30 +0100

    Logging

--- autoapp.py
+++ autoapp.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 """Create an application instance."""
+import logging
 from flask.helpers import get_debug_flag
 
 from tsa.app import create_app
@@ -8,3 +9,10 @@ from tsa.settings import DevConfig, ProdConfig
 CONFIG = DevConfig if get_debug_flag() else ProdConfig
 
 app = create_app(CONFIG)
+
+@app.before_first_request
+def setup_logging():
+    if not app.debug:
+        # In production mode, add log handler to sys.stderr.
+        app.logger.addHandler(logging.StreamHandler())
+        app.logger.setLevel(logging.INFO)
