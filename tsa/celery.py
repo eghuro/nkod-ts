4f21b430ca2cce6d574d592c3d4ebdbf1b66f92b
Alexandr Mansurov <alex@eghuro.cz>
2018-11-15 14:34:42 +0100

    Lint celery

--- tsa/celery.py
+++ tsa/celery.py
@@ -6,7 +6,10 @@ from raven.contrib.celery import register_logger_signal, register_signal
 
 
 class Celery(celery.Celery):
+    """Wrapper over Celery class providing Raven hook for logging."""
+
     def on_configure(self):
+        """Hook Raven for logging."""
         client = raven.Client('https://9df1f926d1854fa4884d1f0ce9489a0b@sentry.io/1304923')
 
         # register a custom filter to filter out duplicate logs
