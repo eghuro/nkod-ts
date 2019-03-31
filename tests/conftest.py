b316cdcee8cea5ab7c978fdd2ad0aa81cddcd38d
Alexandr Mansurov <alex@eghuro.cz>
2019-03-31 10:52:01 +0200

    Fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ import pytest
 from webtest import TestApp
 
 from tsa.app import create_app
-from tsa.database import db as _db
 from tsa.settings import TestConfig
 
 
@@ -25,17 +24,3 @@ def app():
 def testapp(app):
     """A Webtest app."""
     return TestApp(app)
-
-
-@pytest.fixture
-def db(app):
-    """A database for the tests."""
-    _db.app = app
-    with app.app_context():
-        _db.create_all()
-
-    yield _db
-
-    # Explicitly close DB connection
-    _db.session.close()
-    _db.drop_all()
