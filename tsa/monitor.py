df59652005fd38eec032a9fa506eb45d6a76a831
Alexandr Mansurov <alex@eghuro.cz>
2019-04-16 11:33:36 +0200

    Fix test

--- tsa/monitor.py
+++ tsa/monitor.py
@@ -4,11 +4,14 @@ import redis
 class Monitor(object):
     @environment("REDIS")
     def __init__(self, redis_url):
-        self.__client = redis.StrictRedis().from_url(redis_url)
-        for key in self.__client.hkeys('stat:format'):
-            self.__client.hdel('stat:format', key)
-        self.__client.set('stat:sum', 0)
-        self.__client.set('stat:count', 0)
+        try:
+            self.__client = redis.StrictRedis().from_url(redis_url)
+            for key in self.__client.hkeys('stat:format'):
+                self.__client.hdel('stat:format', key)
+            self.__client.set('stat:sum', 0)
+            self.__client.set('stat:count', 0)
+        except redis.exceptions.ConnectionError:
+            self.__client = None
 
 
     def log_format(self, guess):
